2eecaa9f9d3fcd81f3ff8568019a595f656b2f4f
Isaac Iniongun <iniongunisaac@gmail.com>
2023-05-27 22:00:49 +0100

    Initial Commit

---
+++ RandomQuotes/RandomQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuotesApp.swift
+//  RandomQuotes
+//
+//  Created by Isaac Iniongun on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
